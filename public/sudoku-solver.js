5cc894ae5d2b522f5bc01440bccbed5e6219b13a
Glitch (encouraging-brick-rib) <none>
2020-07-22 06:52:58 +0000

    💫💏 Checkpoint ./public/sudoku-solver.js:9596039/2045

--- public/sudoku-solver.js
+++ public/sudoku-solver.js
@@ -4,36 +4,29 @@ import { puzzlesAndSolutions } from './puzzle-strings.js';
 document.addEventListener('DOMContentLoaded', () => {
   // Load a simple puzzle into the text area
   textArea.value = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+  let arr = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+
+  for(let i = 1; i <= textArea.value.length; i++) {
+    let quo = Math.floor(i / 9);
+    let rem = i % 9;
+    if(rem === 0){
+      document.getElementById(`${arr[quo - 1] + 9}`).value = textArea.value[i - 1] === "." ? "" : textArea.value[i - 1];
+    }
+    else {
+      document.getElementById(`${arr[quo] + rem}`).value = textArea.value[i - 1] === "." ? "" : textArea.value[i - 1];
+    }
+  }
 });
 
-textArea.value = "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
 
+textArea.addEventListener('change', (e) => {
+    textArea.value = e.target.value;
+  console.log(textArea.value);
+})
+
+textAr
 /* 
   Export your functions for testing in Node.
   Note: The `try` block is to prevent errors on
   the client side
 */
-
-console.log(textArea.value.length);
-
-let arr = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
-
-for(let i = 1; i <= textArea.value.length; i++) {
-  let quo = Math.floor(i / 9);
-  let rem = i % 9;
-  if(rem === 0){
-    if(textArea.value[i - 1] == ".") {
-      document.getElementById(`${arr[quo - 1] + 9}`).value = "";
-    } else {
-      document.getElementById(`${arr[quo - 1] + 9}`).value = textArea.value[i - 1];
-    }
-  }
-  else {
-    if(textArea.value[i - 1] == ".") {
-      document.getElementById(`${arr[quo - 1] + 9}`).value = "";
-    } else {
-      document.getElementById(`${arr[quo - 1] + 9}`).value = textArea.value[i - 1];
-    }
-  }
-  console.log(i);
-}
